60b8497f3bb647ded208feab6c4217cf501d23b7
tony-ojeda <tojeda96@gmail.com>
2022-02-11 17:38:20 -0500

    change path images

--- src/pages/resume/resume.component.jsx
+++ src/pages/resume/resume.component.jsx
@@ -25,27 +25,27 @@ const Resume = () => {
                 <span className="stars">
                   <img
                     className="star"
-                    src={'./../../../public/assets/icons/instructor/star.svg'}
+                    src={'../assets/icons/instructor/star.svg'}
                     alt=""
                   />
                   <img
                     className="star"
-                    src={'./../../../public/assets/icons/instructor/star.svg'}
+                    src={'../assets/icons/instructor/star.svg'}
                     alt=""
                   />
                   <img
                     className="star"
-                    src={'./../../../public/assets/icons/instructor/star.svg'}
+                    src={'../assets/icons/instructor/star.svg'}
                     alt=""
                   />
                   <img
                     className="star"
-                    src={'./../../../public/assets/icons/instructor/star.svg'}
+                    src={'../assets/icons/instructor/star.svg'}
                     alt=""
                   />
                   <img
                     className="star"
-                    src={'./../../../public/assets/icons/instructor/star.svg'}
+                    src={'../assets/icons/instructor/star.svg'}
                     alt=""
                   />
                 </span>
@@ -53,7 +53,7 @@ const Resume = () => {
               </div>
             </div>
             <div className="blog__image">
-              <img src={'./../../../public/assets/images/instructor/blog1.jpg'} alt="" />
+              <img src={'../assets/images/instructor/blog1.jpg'} alt="" />
             </div>
           </div>
           <div className="blog">
@@ -67,27 +67,27 @@ const Resume = () => {
                 <span className="stars">
                   <img
                     className="star"
-                    src={'./../../../public/assets/icons/instructor/star.svg'}
+                    src={'../assets/icons/instructor/star.svg'}
                     alt=""
                   />
                   <img
                     className="star"
-                    src={'./../../../public/assets/icons/instructor/star.svg'}
+                    src={'../assets/icons/instructor/star.svg'}
                     alt=""
                   />
                   <img
                     className="star"
-                    src={'./../../../public/assets/icons/instructor/star.svg'}
+                    src={'../assets/icons/instructor/star.svg'}
                     alt=""
                   />
                   <img
                     className="star"
-                    src={'./../../../public/assets/icons/instructor/star.svg'}
+                    src={'../assets/icons/instructor/star.svg'}
                     alt=""
                   />
                   <img
                     className="star"
-                    src={'./../../../public/assets/icons/instructor/star.svg'}
+                    src={'../assets/icons/instructor/star.svg'}
                     alt=""
                   />
                 </span>
@@ -95,7 +95,7 @@ const Resume = () => {
               </div>
             </div>
             <div className="blog__image">
-              <img src={'./../../../public/assets/images/instructor/blog2.jpg'} alt="" />
+              <img src={'../assets/images/instructor/blog2.jpg'} alt="" />
             </div>
           </div>
           <div className="blog">
@@ -109,27 +109,27 @@ const Resume = () => {
                 <span className="stars">
                   <img
                     className="star"
-                    src={'./../../../public/assets/icons/instructor/star.svg'}
+                    src={'../assets/icons/instructor/star.svg'}
                     alt=""
                   />
                   <img
                     className="star"
-                    src={'./../../../public/assets/icons/instructor/star.svg'}
+                    src={'../assets/icons/instructor/star.svg'}
                     alt=""
                   />
                   <img
                     className="star"
-                    src={'./../../../public/assets/icons/instructor/star.svg'}
+                    src={'../assets/icons/instructor/star.svg'}
                     alt=""
                   />
                   <img
                     className="star"
-                    src={'./../../../public/assets/icons/instructor/star.svg'}
+                    src={'../assets/icons/instructor/star.svg'}
                     alt=""
                   />
                   <img
                     className="star"
-                    src={'./../../../public/assets/icons/instructor/star.svg'}
+                    src={'../assets/icons/instructor/star.svg'}
                     alt=""
                   />
                 </span>
@@ -137,7 +137,7 @@ const Resume = () => {
               </div>
             </div>
             <div className="blog__image">
-              <img src={'./../../../public/assets/images/instructor/blog3.jpg'} alt="" />
+              <img src={'../assets/images/instructor/blog3.jpg'} alt="" />
             </div>
           </div>
           <div className="blog">
@@ -151,27 +151,27 @@ const Resume = () => {
                 <span className="stars">
                   <img
                     className="star"
-                    src={'./../../../public/assets/icons/instructor/star.svg'}
+                    src={'../assets/icons/instructor/star.svg'}
                     alt=""
                   />
                   <img
                     className="star"
-                    src={'./../../../public/assets/icons/instructor/star.svg'}
+                    src={'../assets/icons/instructor/star.svg'}
                     alt=""
                   />
                   <img
                     className="star"
-                    src={'./../../../public/assets/icons/instructor/star.svg'}
+                    src={'../assets/icons/instructor/star.svg'}
                     alt=""
                   />
                   <img
                     className="star"
-                    src={'./../../../public/assets/icons/instructor/star.svg'}
+                    src={'../assets/icons/instructor/star.svg'}
                     alt=""
                   />
                   <img
                     className="star"
-                    src={'./../../../public/assets/icons/instructor/star.svg'}
+                    src={'../assets/icons/instructor/star.svg'}
                     alt=""
                   />
                 </span>
@@ -179,7 +179,7 @@ const Resume = () => {
               </div>
             </div>
             <div className="blog__image">
-              <img src={'./../../../public/assets/images/instructor/blog4.jpg'} alt="" />
+              <img src={'../assets/images/instructor/blog4.jpg'} alt="" />
             </div>
           </div>
         </div>
@@ -194,7 +194,7 @@ const Resume = () => {
           <div className="profile">
             <div className="profile__photo">
               <img
-                src={'./../../../public/assets/images/instructor/carla.jpg'}
+                src={'../assets/images/instructor/carla.jpg'}
                 alt="profile instructor"
               />
             </div>
@@ -251,7 +251,7 @@ const Resume = () => {
             <div className="instructor">
               <div className="instructor__profile">
                 <img
-                  src={'./../../../public/assets/images/instructor/instructor1.jpg'}
+                  src={'../assets/images/instructor/instructor1.jpg'}
                   alt=""
                 />
                 <div className="instructor__description">
@@ -266,7 +266,7 @@ const Resume = () => {
             <div className="instructor">
               <div className="instructor__profile">
                 <img
-                  src={'./../../../public/assets/images/instructor/instructor2.jpg'}
+                  src={'../assets/images/instructor/instructor2.jpg'}
                   alt=""
                 />
                 <div className="instructor__description">
@@ -281,7 +281,7 @@ const Resume = () => {
             <div className="instructor">
               <div className="instructor__profile">
                 <img
-                  src={'./../../../public/assets/images/instructor/instructor3.jpg'}
+                  src={'../assets/images/instructor/instructor3.jpg'}
                   alt=""
                 />
                 <div className="instructor__description">
@@ -296,7 +296,7 @@ const Resume = () => {
             <div className="instructor">
               <div className="instructor__profile">
                 <img
-                  src={'./../../../public/assets/images/instructor/instructor4.jpg'}
+                  src={'../assets/images/instructor/instructor4.jpg'}
                   alt=""
                 />
                 <div className="instructor__description">
@@ -311,7 +311,7 @@ const Resume = () => {
             <div className="instructor">
               <div className="instructor__profile">
                 <img
-                  src={'./../../../public/assets/images/instructor/instructor5.jpg'}
+                  src={'../assets/images/instructor/instructor5.jpg'}
                   alt=""
                 />
                 <div className="instructor__description">
