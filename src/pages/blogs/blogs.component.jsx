4f55d194da4807c2e02cbdb06d7944faaa07b8b9
tony-ojeda <tojeda96@gmail.com>
2022-02-15 18:21:12 -0500

    pages resume 70% pages blog pages courses

--- src/pages/blogs/blogs.component.jsx
+++ src/pages/blogs/blogs.component.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
-import './blogs.module.scss';
+import Container from '../../../node_modules/react-bootstrap/esm/Container';
+import styles from './blogs.module.scss';
+import { Blog } from 'components/molecules';
 
 const Blogs = () => {
   return (
-    <h1>Hello blogs</h1>
-  )
+    <Container>
+      <h2 className={styles.title}>Blogs publicados</h2>
+      <div className={styles.blogs}>
+        <Blog />
+        <Blog />
+        <Blog />
+        <Blog />
+        <Blog />
+        <Blog />
+        <Blog />
+        <Blog />
+        <Blog />
+      </div>
+    </Container>
+  );
 };
 export default React.memo(Blogs);
--- src/pages/class/class.component.jsx
+++ src/pages/class/class.component.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import { Blog, Course } from 'components/molecules';
+import { Course } from 'components/molecules';
+import styles from './class.module.scss';
+import Container from '../../../node_modules/react-bootstrap/esm/Container';
 
 const Class = () => {
   return (
     <>
-      <Course />
+      <Container>
+        <h2 className={styles.title}>Cursos publicados</h2>
+        <div className={styles.courses}>
+          <Course />
+          <Course />
+          <Course />
+          <Course />
+          <Course />
+          <Course />
+          <Course />
+          <Course />
+          <Course />
+        </div>
+      </Container>
     </>
-  )
+  );
 };
 export default React.memo(Class);
