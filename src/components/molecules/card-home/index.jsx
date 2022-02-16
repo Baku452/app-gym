d799ae1d0d3ef50dfca85117a3cce6b67c60ae17
Aldair Huamani <aiacos12@outlook.com>
2022-02-16 12:44:51 -0500

    add cards to Home pages

--- src/components/molecules/card-home/index.jsx
+++ src/components/molecules/card-home/index.jsx
@@ -4,10 +4,15 @@ import styles from './index.module.scss';
 const CardHome = ({ item }) => {
   return (
     <>
-      <Col className={`${styles.card} p-5`}>
-        <div className={styles.card__overlay} />
-        <img alt={item.title} src={item.img} />
-        <h3>{item.title}</h3>
+      <Col lg={4} className={`${styles.card}`}>
+        <div className={styles.card__content}>
+          <div className={styles.card__overlay} />
+          <img alt={item.title} className={styles.card__img} src={item.img} />
+          <div className={styles.card__textContent}>
+            <h3 className={styles.card__title}>{item.title}</h3>
+            <p className={styles.card__text}>{item.content}</p>
+          </div>
+        </div>
       </Col>
     </>
   );
--- src/data/home/services.js
+++ src/data/home/services.js
@@ -3,15 +3,21 @@ export const services = [
     id: 1,
     title: 'Aerobic',
     img: 'img/trainings/ourTrainings.jpg',
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
   },
   {
     id: 2,
     title: 'Aerobic',
     img: 'img/trainings/ourTrainings_2.jpg',
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
   },
   {
     id: 3,
     title: 'Aerobic',
     img: 'img/trainings/ourTrainings_3.jpg',
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
   },
 ];
