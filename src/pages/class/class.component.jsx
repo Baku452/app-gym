00f8bfa35e875784dd5e5a3b619138b9d344e368
tony-ojeda <tojeda96@gmail.com>
2022-02-20 00:45:05 -0500

    page courses

--- src/pages/class/class.component.jsx
+++ src/pages/class/class.component.jsx
@@ -1,24 +1,188 @@
 import React from 'react';
 import { CardCourse } from 'components/molecules';
 import styles from './class.module.scss';
-import Container from '../../../node_modules/react-bootstrap/esm/Container';
+import { Button } from 'react-bootstrap';
+import Tabs from '../../../node_modules/react-bootstrap/esm/Tabs';
+import { Tab } from 'bootstrap';
+import { BsStar, BsStarFill } from 'react-icons/bs';
+
+//images
+const playIcon = '../assets/icons/instructor/play.svg';
 
 const Class = () => {
   return (
-    <Container>
-      <h2 className={styles.title}>Cursos publicados</h2>
-      <div className={styles.courses}>
-        <CardCourse />
-        <CardCourse />
-        <CardCourse />
-        <CardCourse />
-        <CardCourse />
-        <CardCourse />
-        <CardCourse />
-        <CardCourse />
-        <CardCourse />
+    <div className={styles.principal}>
+      <div className="test">
+        <Tabs defaultActiveKey="saved" id="uncontrolled-tab-example" className="mb-3">
+          <Tab className="custom-tab" eventKey="saved" title="Clases Grabadas">
+            <div className={styles.principal__body}>
+              <div className={styles.principal__courses}>
+                <CardCourse />
+                <CardCourse />
+                <CardCourse />
+                <CardCourse />
+                <CardCourse />
+                <CardCourse />
+                <CardCourse />
+                <CardCourse />
+                <CardCourse />
+              </div>
+            </div>
+          </Tab>
+          <Tab eventKey="direct" title="Clases Directo">
+            <div className={styles.principal__body}>
+              <div className={styles.principal__courses}>
+                <CardCourse />
+                <CardCourse />
+                <CardCourse />
+              </div>
+            </div>
+          </Tab>
+        </Tabs>
       </div>
-    </Container>
+      <div className={styles.principal__courses_best}>
+        <div className={styles.principal__improvement}>
+          <span>Mejores Clases</span>
+          <Button size="sm" style={{ color: '#fff', cursor: 'pointer' }} variant="orange">
+            Ver todo
+          </Button>
+        </div>
+        <div className={styles.principal__courses}>
+          <div className={styles.courses}>
+            <div className={styles.course}>
+              <div className={styles.course__preview}>
+                <img
+                  className={styles.course__image}
+                  src="https://picsum.photos/278/156"
+                  alt="course gym"
+                />
+                <div className={styles.course__play}>
+                  <img class="w-100" src={playIcon} alt="course gym" />
+                </div>
+              </div>
+              <div className={styles.course__description}>
+                <span className={styles.course__title}>
+                  Pectorales de Acero con pesas
+                </span>
+                <span className={styles.course__instructor}>Juan Quino Areche Isra</span>
+                <span className={styles.course__date}>20 Diciembre 2021</span>
+                <span className={styles.starts}>
+                  <BsStarFill fill="#ff8906" />
+                  <BsStarFill fill="#ff8906" />
+                  <BsStarFill fill="#ff8906" />
+                  <BsStar fill="#ff8906" />
+                  <BsStar fill="#ff8906" />
+                </span>
+              </div>
+            </div>
+            <div className={styles.course}>
+              <div className={styles.course__preview}>
+                <img
+                  className={styles.course__image}
+                  src="https://picsum.photos/278/156"
+                  alt="course gym"
+                />
+                <div className={styles.course__play}>
+                  <img class="w-100" src={playIcon} alt="course gym" />
+                </div>
+              </div>
+              <div className={styles.course__description}>
+                <span className={styles.course__title}>
+                  Pectorales de Acero con pesas
+                </span>
+                <span className={styles.course__instructor}>Juan Quino Areche Isra</span>
+                <span className={styles.course__date}>20 Diciembre 2021</span>
+                <span className={styles.starts}>
+                  <BsStarFill fill="#ff8906" />
+                  <BsStarFill fill="#ff8906" />
+                  <BsStarFill fill="#ff8906" />
+                  <BsStar fill="#ff8906" />
+                  <BsStar fill="#ff8906" />
+                </span>
+              </div>
+            </div>
+            <div className={styles.course}>
+              <div className={styles.course__preview}>
+                <img
+                  className={styles.course__image}
+                  src="https://picsum.photos/278/156"
+                  alt="course gym"
+                />
+                <div className={styles.course__play}>
+                  <img class="w-100" src={playIcon} alt="course gym" />
+                </div>
+              </div>
+              <div className={styles.course__description}>
+                <span className={styles.course__title}>
+                  Pectorales de Acero con pesas
+                </span>
+                <span className={styles.course__instructor}>Juan Quino Areche Isra</span>
+                <span className={styles.course__date}>20 Diciembre 2021</span>
+                <span className={styles.starts}>
+                  <BsStarFill fill="#ff8906" />
+                  <BsStarFill fill="#ff8906" />
+                  <BsStarFill fill="#ff8906" />
+                  <BsStar fill="#ff8906" />
+                  <BsStar fill="#ff8906" />
+                </span>
+              </div>
+            </div>
+            <div className={styles.course}>
+              <div className={styles.course__preview}>
+                <img
+                  className={styles.course__image}
+                  src="https://picsum.photos/278/156"
+                  alt="course gym"
+                />
+                <div className={styles.course__play}>
+                  <img class="w-100" src={playIcon} alt="course gym" />
+                </div>
+              </div>
+              <div className={styles.course__description}>
+                <span className={styles.course__title}>
+                  Pectorales de Acero con pesas
+                </span>
+                <span className={styles.course__instructor}>Juan Quino Areche Isra</span>
+                <span className={styles.course__date}>20 Diciembre 2021</span>
+                <span className={styles.starts}>
+                  <BsStarFill fill="#ff8906" />
+                  <BsStarFill fill="#ff8906" />
+                  <BsStarFill fill="#ff8906" />
+                  <BsStar fill="#ff8906" />
+                  <BsStar fill="#ff8906" />
+                </span>
+              </div>
+            </div>
+            <div className={styles.course}>
+              <div className={styles.course__preview}>
+                <img
+                  className={styles.course__image}
+                  src="https://picsum.photos/278/156"
+                  alt="course gym"
+                />
+                <div className={styles.course__play}>
+                  <img class="w-100" src={playIcon} alt="course gym" />
+                </div>
+              </div>
+              <div className={styles.course__description}>
+                <span className={styles.course__title}>
+                  Pectorales de Acero con pesas
+                </span>
+                <span className={styles.course__instructor}>Juan Quino Areche Isra</span>
+                <span className={styles.course__date}>20 Diciembre 2021</span>
+                <span className={styles.starts}>
+                  <BsStarFill fill="#ff8906" />
+                  <BsStarFill fill="#ff8906" />
+                  <BsStarFill fill="#ff8906" />
+                  <BsStar fill="#ff8906" />
+                  <BsStar fill="#ff8906" />
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 export default React.memo(Class);
--- src/pages/resume/resume.component.jsx
+++ src/pages/resume/resume.component.jsx
@@ -1,165 +1,159 @@
 import React from 'react';
 import { CardCourse, CardBlog } from 'components/molecules';
 import styles from './resume.module.scss';
-import Container from '../../../node_modules/react-bootstrap/esm/Container';
 import { Button } from 'react-bootstrap';
 
 const Resume = () => {
   return (
-    <Container>
-      <div className={styles.principal}>
-        <div className={styles.principal__body}>
-          <h2 className={styles.title}>Cursos publicados</h2>
-          <div className={styles.principal__body__courses}>
-            <CardCourse />
-            <CardCourse />
-            <CardCourse />
+    <div className={styles.principal}>
+      <div className={styles.principal__body}>
+        <h2 className={styles.title}>Cursos publicados</h2>
+        <div className={styles.principal__body__courses}>
+          <CardCourse />
+          <CardCourse />
+          <CardCourse />
+        </div>
+        <div class="text-center">
+          <Button size="sm" variant="outline-orange">
+            Ver más
+          </Button>
+        </div>
+        <h2 className={styles.title}>Blogs publicados</h2>
+        <div className={styles.principal__body__blogs}>
+          <CardBlog />
+          <CardBlog />
+        </div>
+        <div class="text-center">
+          <Button size="sm" variant="outline-orange">
+            Ver más
+          </Button>
+        </div>
+      </div>
+      <div className={styles.principal__profiles}>
+        <div className={styles.principal__instructor}>
+          <div className={styles.profile}>
+            <div className={styles.profile__photo}>
+              <img
+                src={'../assets/images/instructor/carla.jpg'}
+                alt="profile instructor"
+              />
+            </div>
+            <div className={styles.profile__name}>
+              <span>Carla Atena</span>
+            </div>
+            <div className={styles.profile__profession}>
+              <span>Personal Trainer</span>
+            </div>
           </div>
-          <div class="text-center">
-            <Button size="sm" variant="outline-orange">
-              Ver más
-            </Button>
-          </div>
-          <h2 className={styles.title}>Blogs publicados</h2>
-          <div className={styles.principal__body__blogs}>
-            <CardBlog />
-            <CardBlog />
-          </div>
-          <div class="text-center">
-            <Button size="sm" variant="outline-orange">
-              Ver más
-            </Button>
+          <div className={styles.resumen}>
+            <div className={styles.row}>
+              <div className={styles.card}>
+                <div className={styles.card__count}>
+                  <span>30</span>
+                </div>
+                <div className={styles.card__title}>
+                  <span>Clases</span>
+                </div>
+              </div>
+              <div className={styles.card}>
+                <div className={styles.card__count}>
+                  <span>120</span>
+                </div>
+                <div className={styles.card__title}>
+                  <span>Inscritos</span>
+                </div>
+              </div>
+              <div className={styles.card}>
+                <div className={styles.card__count}>
+                  <span>35</span>
+                </div>
+                <div className={styles.card__title}>
+                  <span>Blogs</span>
+                </div>
+              </div>
+              <div className={styles.card}>
+                <div className={styles.card__count}>
+                  <span>500</span>
+                </div>
+                <div className={styles.card__title}>
+                  <span>Comentarios</span>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div className={styles.principal__profiles}>
-          <div className={styles.principal__instructor}>
-            <div className={styles.profile}>
-              <div className={styles.profile__photo}>
-                <img
-                  src={'../assets/images/instructor/carla.jpg'}
-                  alt="profile instructor"
-                />
+        <div className={styles.principal__improvement}>
+          <span>Mejores Instructores</span>
+          <Button size="sm" style={{ color: '#fff', cursor: 'pointer' }} variant="orange">
+            Ver todo
+          </Button>
+        </div>
+        <div className={styles.principal__instructors}>
+          <div className={styles.instructors}>
+            <div className={styles.instructor}>
+              <div className={styles.instructor__profile}>
+                <img src={'../assets/images/instructor/instructor1.jpg'} alt="" />
+                <div className={styles.instructor__description}>
+                  <span>Pedro Gonzales</span>
+                  <span>Nutricionista</span>
+                </div>
               </div>
-              <div className={styles.profile__name}>
-                <span>Carla Atena</span>
-              </div>
-              <div className={styles.profile__profession}>
-                <span>Personal Trainer</span>
-              </div>
-            </div>
-            <div className={styles.resumen}>
-              <div className={styles.row}>
-                <div className={styles.card}>
-                  <div className={styles.card__count}>
-                    <span>30</span>
-                  </div>
-                  <div className={styles.card__title}>
-                    <span>Clases</span>
-                  </div>
-                </div>
-                <div className={styles.card}>
-                  <div className={styles.card__count}>
-                    <span>120</span>
-                  </div>
-                  <div className={styles.card__title}>
-                    <span>Inscritos</span>
-                  </div>
-                </div>
-                <div className={styles.card}>
-                  <div className={styles.card__count}>
-                    <span>35</span>
-                  </div>
-                  <div className={styles.card__title}>
-                    <span>Blogs</span>
-                  </div>
-                </div>
-                <div className={styles.card}>
-                  <div className={styles.card__count}>
-                    <span>500</span>
-                  </div>
-                  <div className={styles.card__title}>
-                    <span>Comentarios</span>
-                  </div>
-                </div>
+              <div className={styles.instructor__date}>
+                <span>20 Diciembre 2021</span>
               </div>
             </div>
-          </div>
-          <div className={styles.principal__improvement}>
-            <span>Mejores Instructores</span>
-            <Button
-              size="sm"
-              style={{ color: '#fff', cursor: 'pointer' }}
-              variant="orange">
-              Ver todo
-            </Button>
-          </div>
-          <div className={styles.principal__instructors}>
-            <div className={styles.instructors}>
-              <div className={styles.instructor}>
-                <div className={styles.instructor__profile}>
-                  <img src={'../assets/images/instructor/instructor1.jpg'} alt="" />
-                  <div className={styles.instructor__description}>
-                    <span>Pedro Gonzales</span>
-                    <span>Nutricionista</span>
-                  </div>
-                </div>
-                <div className={styles.instructor__date}>
-                  <span>20 Diciembre 2021</span>
+            <div className={styles.instructor}>
+              <div className={styles.instructor__profile}>
+                <img src={'../assets/images/instructor/instructor2.jpg'} alt="" />
+                <div className={styles.instructor__description}>
+                  <span>Juan Carma</span>
+                  <span>Praparador Fitness</span>
                 </div>
               </div>
-              <div className={styles.instructor}>
-                <div className={styles.instructor__profile}>
-                  <img src={'../assets/images/instructor/instructor2.jpg'} alt="" />
-                  <div className={styles.instructor__description}>
-                    <span>Juan Carma</span>
-                    <span>Praparador Fitness</span>
-                  </div>
-                </div>
-                <div className={styles.instructor__date}>
-                  <span>20 Diciembre 2021</span>
+              <div className={styles.instructor__date}>
+                <span>20 Diciembre 2021</span>
+              </div>
+            </div>
+            <div className={styles.instructor}>
+              <div className={styles.instructor__profile}>
+                <img src={'../assets/images/instructor/instructor3.jpg'} alt="" />
+                <div className={styles.instructor__description}>
+                  <span>Carlos Salas</span>
+                  <span>Suplementación</span>
                 </div>
               </div>
-              <div className={styles.instructor}>
-                <div className={styles.instructor__profile}>
-                  <img src={'../assets/images/instructor/instructor3.jpg'} alt="" />
-                  <div className={styles.instructor__description}>
-                    <span>Carlos Salas</span>
-                    <span>Suplementación</span>
-                  </div>
-                </div>
-                <div className={styles.instructor__date}>
-                  <span>20 Diciembre 2021</span>
+              <div className={styles.instructor__date}>
+                <span>20 Diciembre 2021</span>
+              </div>
+            </div>
+            <div className={styles.instructor}>
+              <div className={styles.instructor__profile}>
+                <img src={'../assets/images/instructor/instructor4.jpg'} alt="" />
+                <div className={styles.instructor__description}>
+                  <span>Pedro Gonzales</span>
+                  <span>Nutricionista</span>
                 </div>
               </div>
-              <div className={styles.instructor}>
-                <div className={styles.instructor__profile}>
-                  <img src={'../assets/images/instructor/instructor4.jpg'} alt="" />
-                  <div className={styles.instructor__description}>
-                    <span>Pedro Gonzales</span>
-                    <span>Nutricionista</span>
-                  </div>
-                </div>
-                <div className={styles.instructor__date}>
-                  <span>20 Diciembre 2021</span>
+              <div className={styles.instructor__date}>
+                <span>20 Diciembre 2021</span>
+              </div>
+            </div>
+            <div className={styles.instructor}>
+              <div className={styles.instructor__profile}>
+                <img src={'../assets/images/instructor/instructor5.jpg'} alt="" />
+                <div className={styles.instructor__description}>
+                  <span>Pedro Gonzales</span>
+                  <span>Nutricionista</span>
                 </div>
               </div>
-              <div className={styles.instructor}>
-                <div className={styles.instructor__profile}>
-                  <img src={'../assets/images/instructor/instructor5.jpg'} alt="" />
-                  <div className={styles.instructor__description}>
-                    <span>Pedro Gonzales</span>
-                    <span>Nutricionista</span>
-                  </div>
-                </div>
-                <div className={styles.instructor__date}>
-                  <span>20 Diciembre 2021</span>
-                </div>
+              <div className={styles.instructor__date}>
+                <span>20 Diciembre 2021</span>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </Container>
+    </div>
   );
 };
 export default React.memo(Resume);
