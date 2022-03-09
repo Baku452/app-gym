a9387d494c6703454b90adefb58d29316bd78f2e
Aldair Huamani <aiacos12@outlook.com>
2022-03-09 18:16:35 -0500

    push dashboard user

---
+++ src/layout/dashboard-student/dashboard-student.component.jsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { Outlet } from 'react-router';
+import Container from '../../node_modules/react-bootstrap/esm/Container';
+import { Sidebar, Header } from './../components/organisms';
+// import styles from './dashboard-admin.module.scss';
+
+export const DashboardStudent = () => {
+  return (
+    <div className="d-flex">
+      <Sidebar />
+      <div className="w-100">
+        <Header />
+        <div className="body">
+          <Container>
+            <Outlet />
+          </Container>
+        </div>
+      </div>
+    </div>
+  );
+};
