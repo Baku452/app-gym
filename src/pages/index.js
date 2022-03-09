2cb99d1196d2a5be856092b98596994a06d63f15
Aldair Huamani <aiacos12@outlook.com>
2022-03-09 18:50:03 -0500

    fix 2.0

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Courses from './courses/courses.component';
 import Configs from './configs/configs.component';
 import Resume from './resume/resume.component';
 import Students from './students/students.component';
-import WebHome from './home/web-home.page.jsx';
+import WebHome from './home/web-home.page';
 import Login from './login/index';
 
 export { WebHome, Login, Blogs, Courses, Configs, Resume, Students };
